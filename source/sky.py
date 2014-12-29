c0c2df360d8ef0534b5d126528754682eaf82485
Peter Denno <podenno@gmail.com>
2014-12-29 12:24:50 -0500

    Update for Python 3. Correct pathname to images directory.

--- source/sky.py
+++ source/sky.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#!/home/pdenno/anaconda3/bin/python
 
-import Tkinter as tk
+import tkinter as tk
 import random
-import os
+#import os
 
 GAME = False
 TILE_X_SIZE = 60
@@ -29,14 +29,14 @@ class Tile(Thing):
     
     def draw(self):
         "Place the widget on the screen."
-        #print "Drawing {0} at x = {1}, y={2}".format(self,TILE_X_SIZE*((self.col)-1),TILE_Y_SIZE*((self.row)-1))
-        #print "Drawing {0}, self.state = {1}".format(self,self.state)
+        #"Drawing {0} at x = {1}, y={2}".format(self,TILE_X_SIZE*((self.col)-1),TILE_Y_SIZE*((self.row)-1))
+        #"Drawing {0}, self.state = {1}".format(self,self.state)
         if self.state == 0:
             self.widget.configure(image = self.off_image)
             self.widget.place_forget()
             self.widget.place(x=TILE_X_SIZE*(self.col), y=TILE_Y_SIZE*(self.row))
         else:  #  self.state == 1
-            print "Tile ({0},{1}) state is {2}.".format(self.row, self.col, self.state)
+            "Tile ({0},{1}) state is {2}.".format(self.row, self.col, self.state)
             self.widget.configure(image = self.on_image)
             self.widget.place_forget()
             self.widget.place(x=TILE_X_SIZE*(self.col), y=TILE_Y_SIZE*(self.row))
@@ -45,14 +45,14 @@ class Tile(Thing):
         self.widget.place_forget()
         self._drop_position = self._drop_position + 1
         self.widget.place(x=TILE_X_SIZE*(self.col), y=TILE_Y_SIZE*(self.row-1) + self._drop_position)
-        self._drop_job_id = GAME.root.after(3, self.drop)
+        self._drop_job_id = GAME.root.after(20, self.drop)
         if self._drop_position >= TILE_Y_SIZE:
             GAME.root.after_cancel(self._drop_job_id)
             self._drop_position = 0
             
     def buttonPress(self):
         "If off, turn on. If on, turn off by changing state and image."
-        print "\n\nStart button press {0}, self.state = {1}".format(self,self.state)        
+        "\n\nStart button press {0}, self.state = {1}".format(self,self.state)        
         if self.state == 0:
             self.state = 1
             self.draw()
@@ -63,13 +63,12 @@ class Tile(Thing):
         else:
             self.state = 0 
             self.draw()
-            print "End button press {0}, self.state = {1}".format(self,self.state)
+            "End button press {0}, self.state = {1}".format(self,self.state)
             
     def maybeSwap(self):
         '''Check tiles next to self. If ON, then swap and turn both off'''
-        print 'check for swap'
         near_me = self.neighbors()
-        print "neighbors = {0}".format(near_me)
+        "neighbors = {0}".format(near_me)
         for near in near_me:
             if near.state == 1:
                 hold_row = self.row
@@ -90,7 +89,7 @@ class Tile(Thing):
         n = False
         r = self.row
         c = self.col
-        print "r = {0} c = {1}".format(r, c)
+        "r = {0} c = {1}".format(r, c)
         last_row = GAME.nrows-1
         last_col = GAME.ncols-1
         # first see if it is a corner...
@@ -120,23 +119,23 @@ class Tile(Thing):
 # that the game item is to appear on, and sets the 'off' and 'on' images.
 class Bird(Tile):
     def __init__(self,irow,icol):
-        super(Bird,self).__init__(irow,icol,"images/bird60OFF.gif", "images/bird60ON.gif")
+        super(Bird,self).__init__(irow,icol,"../images/bird60OFF.gif", "../images/bird60ON.gif")
 
 class Cloud(Tile):
     def __init__(self,irow,icol):
-        super(Cloud,self).__init__(irow,icol,"images/cloud60OFF.gif","images/cloud60ON.gif")
+        super(Cloud,self).__init__(irow,icol,"../images/cloud60OFF.gif","../images/cloud60ON.gif")
 
 class Snow(Tile): 
     def __init__(self,irow,icol):
-        super(Snow,self).__init__(irow,icol,"images/snow60OFF.gif","images/snow60ON.gif")
+        super(Snow,self).__init__(irow,icol,"../images/snow60OFF.gif","../images/snow60ON.gif")
 
 class Rain(Tile): 
     def __init__(self,irow,icol):
-        super(Rain,self).__init__(irow,icol,"images/rain60OFF.gif","images/rain60ON.gif")
+        super(Rain,self).__init__(irow,icol,"../images/rain60OFF.gif","../images/rain60ON.gif")
 
 class Sun(Tile): 
     def __init__(self,irow,icol):
-        super(Sun,self).__init__(irow,icol,"images/sun60OFF.gif","images/sun60ON.gif")
+        super(Sun,self).__init__(irow,icol,"../images/sun60OFF.gif","../images/sun60ON.gif")
 
         
 class Game (object):
@@ -149,12 +148,10 @@ class Game (object):
         self.start_gui()
     def start_gui (self):
         "Do basic window setup."
-        os.chdir("/home/pdenno/new-projects/owen/saveTheSky")
         self.root = tk.Tk()
-        self.root.geometry("480x320+200+200")
+        self.root.geometry("480x320+0+0") # Size of our Raspberry Pi screen
         self.canvas = tk.Canvas(self.root, width = 480, height = 320, bg = 'LightBlue')
         self.canvas.pack()
-        self.empty_image = tk.PhotoImage(file="images/black.gif") # <=== all black for temporarily empty area
         for i in range(self.nrows): # Walk through the rows
             row = []
             for j in range(self.ncols): # Walk through the columns
@@ -173,7 +170,7 @@ class Game (object):
         for i in range(self.nrows):
             for j in range(self.ncols):
                  self.grid[i][j].draw()
-        print "grid = {0}".format(self.grid)
+        "grid = {0}".format(self.grid)
         self.root.mainloop()
     def three_in_a_row(self):
         '''Return a list of tiles that end a sequence of three or more in a row.'''
@@ -206,7 +203,7 @@ class Game (object):
     def dropTiles(self,start_tile):
         '''Drop tiles above the start_tile and every tile of its type to the left.
         all the way to the top row. Then fill in top row with new tiles.'''
-        print "Dropping starting at {0}".format(start_tile)
+        "Dropping starting at {0}".format(start_tile)
         irow = start_tile.row
         icol = start_tile.col
         len = 3
@@ -216,11 +213,9 @@ class Game (object):
             len = 5
         for i in range (irow-1,-1,-1):
             for j in range(icol,icol-len,-1):
-                print "  Dropping into ({0},{1}), state = {2}".format(i+1, j, self.grid[i+1][j].state)
+                " \n Dropping into ({0},{1}), state = {2}".format(i+1, j, self.grid[i+1][j].state)
                 self.grid[i][j].row = i+1
                 self.grid[i+1][j] = self.grid[i][j]
                 self.grid[i+1][j].drop()
-            
     
-
-
+#Game()
